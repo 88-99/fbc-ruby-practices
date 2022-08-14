a41eed5b7bc44c2ec9b7103cadcb85149bb823ee
kw <xz835.525o39@yahoo.com>
2022-08-15 00:04:48 +0900

    ボウリングのスコア計算プログラムを実装

---
+++ 04.bowling/bowling.rb
@@ -0,0 +1,55 @@
+score = ARGV[0]
+scores = score.split(',')
+shots = []
+scores.each do |s|
+  if s == 'X'
+    shots << 10
+    shots << 0
+  else
+    shots << s.to_i
+  end
+end
+
+frames = []
+shots.each_slice(2) do |s|
+  frames << s
+end
+
+frames.each do |frame|
+  if frame == [10, 0]
+    frame.delete(0)
+  end
+end
+
+strikes = []
+spares = []
+frames.first(9).each_with_index do |frame, i|
+  if frame == [10]
+    strikes << i
+  elsif frame.sum == 10
+    spares << i
+  end
+end
+
+basic_points = []
+frames.each do |frame|
+  basic_points << frame.sum
+end
+
+strike_points = []
+strikes.each do |strike|
+  if frames[strike] == [10]
+    if frames[strike + 1] == [10]
+      strike_points << (frames[strike + 1][0] + frames[strike + 2][0])
+    else
+      strike_points << frames[strike + 1].sum
+    end
+  end
+end
+
+spare_points = []
+spares.each do |spare|
+  spare_points << (frames[spare + 1][0])
+end
+
+p (basic_points + strike_points + spare_points).sum
