51e2c81464c9bb117ff6b1eff579741255961a19
88-99 <xz835.525o39@yahoo.com>
2023-04-16 17:28:23 +0900

    sumメソッドの使い方を変更してストライクとスペア計算メソッドを修正中。if文でエラー。

--- 07.bowling_object/lib/frame.rb
+++ 07.bowling_object/lib/frame.rb
@@ -8,27 +8,46 @@ class Frame
   def initialize(index, frames, first_mark, second_mark = nil)
     @first_shot = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark)
-    @next_frame = frames[index + 1]
-    @after_next_frame = frames[index + 2]
+    @next_frame = frames[index + 1].nil? ? [0, 0] : frames[index + 1]
+    @after_next_frame = frames[index + 2].nil? ? [0, 0] : frames[index + 2]
+# 元のコード
+    # @next_frame = frames[index + 1]
+    # @after_next_frame = frames[index + 2]
   end
 
   def self.calc_total_strike(frames)
-    strikes = []
-    frames.first(9).each do |fr|
-      if fr.first_shot.mark == 10 && fr.next_frame[0] == 10
-        strikes << [fr.next_frame[0], fr.after_next_frame[0]].sum
-      elsif fr.first_shot.mark == 10
-        strikes << fr.next_frame.sum
-      end
+    frames.first(9).sum do |fr|
+      # if fr.first_shot.mark == 10 && fr.next_frame[0] == 10
+        fr.next_frame[0] + fr.after_next_frame[0]
+      # elsif fr.first_shot.mark == 10
+        # fr.next_frame.sum
+      # end
     end
-    strikes.sum
+
+# 元のコード
+    # strikes = []
+    # frames.first(9).each do |fr|
+    #   if fr.first_shot.mark == 10 && fr.next_frame[0] == 10
+    #     strikes << [fr.next_frame[0], fr.after_next_frame[0]].sum
+    #   elsif fr.first_shot.mark == 10
+    #     strikes << fr.next_frame.sum
+    #   end
+    # end
+    # strikes.sum
   end
 
   def self.calc_total_spare(frames)
-    spares = []
-    frames.first(9).each do |fr|
-      spares << fr.next_frame[0] if [fr.first_shot.mark, fr.second_shot.mark].compact.sum == 10 && !fr.second_shot.mark.zero?
+    frames.first(9).sum do |fr|
+      fr.next_frame[0] # if [fr.first_shot.mark, fr.second_shot.mark].compact.sum == 10 && !fr.second_shot.mark.zero?
     end
-    spares.sum
   end
+
+# 元のコード
+  # def self.calc_total_spare(frames)
+  #   spares = []
+  #   frames.first(9).each do |fr|
+  #     spares << fr.next_frame[0] if [fr.first_shot.mark, fr.second_shot.mark].compact.sum == 10 && !fr.second_shot.mark.zero?
+  #   end
+  #   spares.sum
+  # end
 end
