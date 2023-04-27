096240a6901b49d5b6fb9a603473cfbd1264a8c3
88-99 <xz835.525o39@yahoo.com>
2023-04-28 07:16:23 +0900

    ストライクとスペア合計の計算メソッドをGameクラスからFrameクラスに戻す

--- 07.bowling_object/lib/frame.rb
+++ 07.bowling_object/lib/frame.rb
@@ -17,4 +17,27 @@ class Frame
   def spare?
     [@first_shot.score, @second_shot.score].compact.sum == 10 && !@second_shot.score.zero?
   end
+
+  def self.calc_total_strike(frames)
+    strikes = []
+    frames.first(9).each_with_index do |frame, i|
+      next unless frame.strike?
+
+      strikes << frames[i + 1].first_shot.score
+      strikes << if frames[i + 1].strike?
+                   frames[i + 2].first_shot.score
+                 else
+                   frames[i + 1].second_shot.score
+                 end
+    end
+    strikes.sum
+  end
+
+  def self.calc_total_spare(frames)
+    spares = []
+    frames.first(9).each_with_index do |frame, i|
+      spares << frames[i + 1].first_shot.score if frame.spare?
+    end
+    spares.sum
+  end
 end
--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -37,26 +37,11 @@ class Game
   end
 
   def calc_total_strike(frames)
-    strikes = []
-    frames.first(9).each_with_index do |frame, i|
-      next unless frame.strike?
-
-      strikes << frames[i + 1].first_shot.score
-      strikes << if frames[i + 1].strike?
-                   frames[i + 2].first_shot.score
-                 else
-                   frames[i + 1].second_shot.score
-                 end
-    end
-    strikes.sum
+    Frame.calc_total_strike(frames)
   end
 
   def calc_total_spare(frames)
-    spares = []
-    frames.first(9).each_with_index do |frame, i|
-      spares << frames[i + 1].first_shot.score if frame.spare?
-    end
-    spares.sum
+    Frame.calc_total_spare(frames)
   end
 
   def calc_total_score
