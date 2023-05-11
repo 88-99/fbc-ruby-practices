083e3286582664a4d38597268855846c049ea152
88-99 <xz835.525o39@yahoo.com>
2023-05-12 08:03:11 +0900

    テストを修正

--- 08.ls_object/test/test.rb
+++ 08.ls_object/test/test.rb
@@ -18,28 +18,8 @@ class LsTest < Minitest::Test
     Ls.new
   end
 
-  def test_parse_option
-    assert_equal({ a: true, r: true }, ls.send(:parse_option, ARGV.replace(['-ar'])))
-  end
-
-  def test_apply_option_to_filenames
-    assert_equal ['12345.txt', '99991111.txt', 'a.txt', 'b.txt', 'dir1', 'dir2', 'dir3'], ls.send(:apply_option_to_filenames)
-  end
-
-  def test_apply_option_to_filenames_with_r
-    ARGV.replace(['-r'])
-
-    assert_equal ['dir3', 'dir2', 'dir1', 'b.txt', 'a.txt', '99991111.txt', '12345.txt'], ls.send(:apply_option_to_filenames)
-  end
-
-  def test_determine_row_indexes
-    assert_equal [1, 2], ls.send(:determine_row_indexes)
-  end
-
-  def test_transpose_safely
-    ARGV.replace(['-a'])
-
-    assert_equal [['.', 'a.txt', 'dir2'], ['12345.txt', 'b.txt', 'dir3'], ['99991111.txt', 'dir1', nil]], ls.send(:transpose_safely)
+  def ls_l
+    LOption.new(ls.send(:apply_option_to_filenames))
   end
 
   def test_build_form_except_l_option
@@ -49,14 +29,12 @@ class LsTest < Minitest::Test
 
   def test_total_file_blocks
     ARGV.replace(['-a'])
-    lop = LOption.new(ls.send(:apply_option_to_filenames))
 
-    assert_equal 'total 16', lop.total_file_blocks
+    assert_equal 'total 16', ls_l.total_file_blocks
   end
 
   def test_l_option
     ARGV.replace(['-ar'])
-    lop = LOption.new(ls.send(:apply_option_to_filenames))
 
     assert_equal ['drwxr-xr-x  2 kw  staff   64  5 11 14:48 dir3',
                   'drwxr-xr-x  2 kw  staff   64  5 11 14:48 dir2',
@@ -65,6 +43,6 @@ class LsTest < Minitest::Test
                   '-rw-r--r--  1 kw  staff    0  5 11 14:48 a.txt',
                   '-rw-r--r--  1 kw  staff   10  5 12 07:37 99991111.txt',
                   '-rw-r--r--  1 kw  staff    5  5 12 07:38 12345.txt',
-                  'drwxr-xr-x  9 kw  staff  288  5 11 14:48 .'], lop.l_option
+                  'drwxr-xr-x  9 kw  staff  288  5 11 14:48 .'], ls_l.l_option
   end
 end
