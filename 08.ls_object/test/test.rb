0ca6ab469b29ed7529bfcaa93a456ea2b603946b
88-99 <xz835.525o39@yahoo.com>
2023-05-12 07:43:03 +0900

    テストを追加

--- 08.ls_object/test/test.rb
+++ 08.ls_object/test/test.rb
@@ -46,4 +46,25 @@ class LsTest < Minitest::Test
     assert_equal ['12345.txt       b.txt           dir3        ', '99991111.txt    dir1        ', 'a.txt           dir2        '],
                  ls.send(:build_form_except_l_option)
   end
+
+  def test_total_file_blocks
+    ARGV.replace(['-a'])
+    lop = LOption.new(ls.send(:apply_option_to_filenames))
+
+    assert_equal 'total 16', lop.total_file_blocks
+  end
+
+  def test_l_option
+    ARGV.replace(['-ar'])
+    lop = LOption.new(ls.send(:apply_option_to_filenames))
+
+    assert_equal ['drwxr-xr-x  2 kw  staff   64  5 11 14:48 dir3',
+                  'drwxr-xr-x  2 kw  staff   64  5 11 14:48 dir2',
+                  'drwxr-xr-x  2 kw  staff   64  5 11 14:48 dir1',
+                  '-rw-r--r--  1 kw  staff    0  5 11 14:48 b.txt',
+                  '-rw-r--r--  1 kw  staff    0  5 11 14:48 a.txt',
+                  '-rw-r--r--  1 kw  staff   10  5 12 07:37 99991111.txt',
+                  '-rw-r--r--  1 kw  staff    5  5 12 07:38 12345.txt',
+                  'drwxr-xr-x  9 kw  staff  288  5 11 14:48 .'], lop.l_option
+  end
 end
